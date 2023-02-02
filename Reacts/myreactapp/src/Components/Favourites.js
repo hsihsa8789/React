1071d0d3db905b46da7c576a2d7f6b9388315207
Ashiah kumar <ashish.dubey8789@gmail.com>
2023-02-02 16:38:33 +0530

    fav-remove

--- Reacts/myreactapp/src/Components/Favourites.js
+++ Reacts/myreactapp/src/Components/Favourites.js
@@ -8,7 +8,9 @@ export default class Favourites extends Component {
             movies : [],
             genre : [],
             currGener : "All Genre",
-            currText : " ",
+            currText : "",
+            limit: 5,
+            currPage : 1,
         }
     } 
 
@@ -68,6 +70,58 @@ export default class Favourites extends Component {
       });
     }
 
+    sortPopularityAsc = () => {
+      let allMovies = this.state.movies;
+      allMovies.sort((objA,objB) => {
+        return objA.popularity - objB.popularity;
+      })
+      this.setState({
+        movies : [...allMovies],
+      })
+    }
+    sortPopularityDesc = () => {
+      let allMovies = this.state.movies;
+      allMovies.sort((objA,objB) => {
+        return objB.popularity - objA.popularity;
+      })
+      this.setState({
+        movies : [...allMovies],
+      })
+    }
+    sortRstingAsc = () => {
+      let allMovies = this.state.movies;
+      allMovies.sort((objA,objB) => {
+        return objB.vote_average - objA.vote_average;
+      })
+      this.setState({
+        movies : [...allMovies],
+      })
+    }
+    sortRstingDesc = () => {
+      let allMovies = this.state.movies;
+      allMovies.sort((objA,objB) => {
+        return objA.vote_average - objB.vote_average;
+      })
+      this.setState({
+        movies : [...allMovies],
+      })
+    }
+
+    handelePageNum = (page) =>{
+      this.setState({
+        currPage: page,
+      })
+    }
+
+    handleDelete = (id) => {
+      let newMovies = this.state.movies.filter((movieObj) => {
+        return movieObj.id != id;
+      })
+      this.setState({
+        movies:[...newMovies]
+      })
+      localStorage.setItem("movies",JSON.stringify(newMovies));
+    }
       
   render() {
     let genreId = {
@@ -91,30 +145,55 @@ export default class Favourites extends Component {
         10752: "War",
         37: "Western",
       };
+    let filteredMovies = this.state.movies;
 
-    let filteredMovies = [];
+    if(this.state.currText === ""){
+      filteredMovies = this.state.movies;
+    }else{
+      filteredMovies = filteredMovies.filter((movieObj) =>{
+        let movieName = movieObj.original_title.toLowerCase();
+        return movieName.includes(this.state.currText);
+      })
+    }
+
+   
     if(this.state.currGener != "All Genre"){
       filteredMovies = this.state.movies.filter((movieObj) => genreId[movieObj.genre_ids[0]] == this.state.currGener);
-    }else{
-      filteredMovies = this.state.movies;
     }
+
+    let numOfPages = Math.ceil(filteredMovies.length/this.state.limit);
+    let pageArr = [];
+
+    for(let i=1;i<=numOfPages;i++){
+      pageArr.push(i);
+    }
+
+    let si = (this.state.currPage-1)*this.state.limit;
+    let ei = si + this.state.limit - 1;
+    filteredMovies = filteredMovies.slice(si,ei+1);
+
 
     return (
     <div class="row">
         <div class="col-3">
           <ul class="list-group">
-            {this.state.genre.map((genre) => (
-               this.state.currGener == genre ?  
-               <li class="list-group-item active" aria-current="true">{genre}</li>:
-               <li class="list-group-item" aria-current="true" onClick={() => this.handleCurrGenre(genre)}>{genre}</li>
-           ))}
+            {this.state.genre.map((genre) => 
+               this.state.currGener == genre ? (
+               <li class="list-group-item active" aria-current="true">
+                {genre}</li>
+                ):(
+                <li class="list-group-item" aria-current="true"
+                 onClick={() => this.handleCurrGenre(genre)}>
+                  {genre}</li>
+             )
+             )}
            
           </ul>
         </div>
           <div class="col">
             <div class="row">
                 <input type="text" className="col" placeholder="Search" value={this.state.currText} onChange={this.handleText}></input>
-                <input type="number" className="col" placeholder="5"></input>
+                <input type="number" className="col-4" value={this.state.limit} onChange={(e)=>this.setState({limit:e.target.value})}></input>
             </div>
             <div class="row">
          <table class="table">
@@ -123,11 +202,14 @@ export default class Favourites extends Component {
                    <th scope="col">Title</th>
                    <th scope="col">Genere</th>
                    
-                   <th scope="col"><i class="fa-solid fa-caret-up"/>Popularity
-                   <i class="fa-solid fa-caret-down"/></th>
+                   <th scope="col">
+                    <i class="fa-solid fa-caret-up" onClick={this.sortPopularityAsc}/>
+                    Popularity
+                   <i class="fa-solid fa-caret-down"onClick={this.sortPopularityDesc}/></th>
 
-                   <th scope="col"><i class="fa-solid fa-caret-up"/>Rating
-                   <i class="fa-solid fa-caret-down"/></th>
+                   <th scope="col"><i class="fa-solid fa-caret-up"onClick={this.sortRstingAsc}/>
+                    Rating
+                   <i class="fa-solid fa-caret-down"onClick={this.sortRstingDesc}/></th>
 
                    <th scope="col"></th>
                  </tr>
@@ -146,7 +228,17 @@ export default class Favourites extends Component {
             </table>
             </div>  
           </div>
-
+          <nav aria-label='Page navigation example'>
+            <ul class="pagination">
+              {pageArr.map((page)=>(
+                <li class="page-item">
+                  <a class="page-link" onClick={() => this.handelePageNum(page)}>
+                    {page}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
 
         </div>
     )
